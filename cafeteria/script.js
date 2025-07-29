5265382dc122e2e6156a16f7e4063028a1d21d93
Diogolsn10 <dogodiogo09@gmail.com>
2025-07-29 21:51:52 +0100

    Finalizado com animação

--- cafeteria/script.js
+++ cafeteria/script.js
@@ -10,4 +10,14 @@ function escondermodal() {
     mascara.style.visibility = "hidden";
 }
 
+document.querySelectorAll('.scroll-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
 
